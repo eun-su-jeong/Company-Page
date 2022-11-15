bce7363e63ada7962978bbc61bef23dbb14fdb7c
eunjjang1 <sjeun@piino.co.kr>
2022-11-15 09:58:43 +0900

    overlay menu

--- js/common.js
+++ js/common.js
@@ -22,8 +22,8 @@ var _device = {};
 
 	// platform & browser detection
 	var ua = navigator.userAgent.toLowerCase();
-	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua)
-		|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0, 4))) {
+	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(ua) ||
+		/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0, 4))) {
 		_device.isMobile = true;
 		_device.isAndroid = ua.indexOf("android") > -1;
 		_device.isiOS = /iPhone/.test(navigator.userAgent) && !window.MSStream;
@@ -35,9 +35,11 @@ var _device = {};
 		_device.isEdge = !_device.isIE && !!window.StyleMedia;
 		_device.isEdgeChromium = _device.isChrome && (navigator.userAgent.indexOf("Edg") != -1);
 		_device.isFirefox = typeof InstallTrigger !== "undefined";
-		_device.isIE = /*@cc_on!@*/false || !!document.documentMode;
+		_device.isIE = /*@cc_on!@*/ false || !!document.documentMode;
 		_device.isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0;
-		_device.isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window["safari"] || (typeof safari !== "undefined" && safari.pushNotification));
+		_device.isSafari = /constructor/i.test(window.HTMLElement) || (function (p) {
+			return p.toString() === "[object SafariRemoteNotification]";
+		})(!window["safari"] || (typeof safari !== "undefined" && safari.pushNotification));
 	}
 
 	// custom event triggering
@@ -51,25 +53,36 @@ var publish = function () {
 	var common = {
 		init: function () {
 			common.insta();
+			common.toggleBtn();
 			// common.carousel();
 		},
-		insta: function() {
+		toggleBtn: function () {
+			$('.btn-menu').click(function () {
+				let text = $('.lbl');
+				$('.toggle').toggleClass('active');
+				$('.overlay').toggleClass('open');
+				if ($('.toggle').hasClass('active')) {
+					text.html("CLOSE");
+				} else {
+					text.html("MENU");
+				}
+			});
+		},
+		insta: function () {
 			var $placeholder = $(".instagram_placeholder"),
 				placeholder = $placeholder.get(0),
 				observe = true,
 				len = [];
 			const feed = new Instafeed({
 				limit: 1000,
-				accessToken:
-				"IGQVJWYWhra2E3MGVtNW1WRm9vVTQ5NlVaTHZACUnJkZAnFVcTZAyd3NGRGZAlQUhIMHNIVk5fTEJOaUlIYlIyMHduVE1wMXBWeUF0Ml9QMFRNTFR0eW95eXd6NmFCd2owblZAqTl9hYXoxeDk5M29vOU1NbgZDZD",
+				accessToken: "IGQVJWYWhra2E3MGVtNW1WRm9vVTQ5NlVaTHZACUnJkZAnFVcTZAyd3NGRGZAlQUhIMHNIVk5fTEJOaUlIYlIyMHduVE1wMXBWeUF0Ml9QMFRNTFR0eW95eXd6NmFCd2owblZAqTl9hYXoxeDk5M29vOU1NbgZDZD",
 				target: placeholder,
-				template:
-				"<a href=\"{{link}}\" class=\"item\">" +
-				"<img title=\"{{caption}}\" src=\"{{image}}\"/>" +
-				"<div class=\"desc\">{{caption}}</div>" +
-				"</a>"
+				template: "<a href=\"{{link}}\" class=\"item\">" +
+					"<img title=\"{{caption}}\" src=\"{{image}}\"/>" +
+					"<div class=\"desc\">{{caption}}</div>" +
+					"</a>"
 			});
-			feed.run(function() {
+			feed.run(function () {
 				console.log("length", $(".swiper-slide").length);
 			});
 			len[0] = $(".item", $placeholder).length;
@@ -84,20 +97,22 @@ var publish = function () {
 				childList: true,
 				subtree: true
 			});
+
 			function getSwiper() {
 				var $item = $(".item", $placeholder),
 					$group = $(),
 					feed = 8;
 				console.log("$item.length", $item.length)
-				$item.each(function(i) {
+				$item.each(function (i) {
 					$group = $group.add(this);
 					if ((((i + 1) % feed) == 0) || (i == ($item.length - 1))) {
-						$group.wrapAll("<li class=\"swiper-slide\"><div class=\"items\"></div></li>"); 
-						$group = $(); 
+						$group.wrapAll("<li class=\"swiper-slide\"><div class=\"items\"></div></li>");
+						$group = $();
 					}
 				});
 				setSwiper();
 			}
+
 			function setSwiper() {
 				swiper = new Swiper(".instafeed-swiper .swiper-container", {
 					direction: "vertical",
@@ -117,7 +132,7 @@ var publish = function () {
 				});
 			}
 		},
-		carousel: function() {
+		carousel: function () {
 			var config = {
 				ui: ".viewer-carousel",
 				slidesPerView: 1,
@@ -132,6 +147,7 @@ var publish = function () {
 				paginationType: "bullets"
 			}
 			carousel = setCarousel(config);
+
 			function setCarousel(config) {
 				var $html = $("html"),
 					$ui = $(config.ui),
@@ -171,8 +187,9 @@ var publish = function () {
 							transitionEnd: onTransitionEnd
 						}
 					});
+
 					function onInit() {
-						setTimeout(function() {
+						setTimeout(function () {
 							var $lbl = $(".lbl", $pp);
 							if ($ui.hasClass("paused")) {
 								swiper.autoplay.stop();
@@ -196,6 +213,7 @@ var publish = function () {
 							$ui.toggleClass("has-only-slide", ($li.length == 1));
 						}, 100)
 					}
+
 					function onTransitionEnd() {
 						var $slide = $(".swiper-slide", $ui),
 							$active = $slide.filter(".swiper-slide-active"),
@@ -204,15 +222,15 @@ var publish = function () {
 						$html.toggleClass("guide-on", $active.hasClass("has-guide"));
 						if ($overlay.length > 0) {
 							var $btns = $("<div class=\"btns\"></div>").appendTo($control),
-							$btn = $("<a href=\"#\" class=\"btn-sheet\"></a>").appendTo($btns);
-							$btn.on("click", function() {
+								$btn = $("<a href=\"#\" class=\"btn-sheet\"></a>").appendTo($btns);
+							$btn.on("click", function () {
 								$html.addClass("overlay-on");
 							});
 							var $sheet = $(".sheet", $overlay);
 							if ($sheet.hasClass("has-second")) {
 								var $btns2 = $("<div class=\"btns2\"></div>").appendTo($control),
 									$btn2 = $("<a href=\"#\" class=\"btn-second\"></a>").appendTo($btns2);
-								$btn2.on("click", function() {
+								$btn2.on("click", function () {
 									$sheet.addClass("second-on");
 									$btns2.remove();
 								});
@@ -223,11 +241,11 @@ var publish = function () {
 						}
 						$html.toggleClass("chat-on", $active.hasClass("has-chat"));
 						if ($active.hasClass("has-chat")) {
-							$(".ui-chat", $active).each(function() {
+							$(".ui-chat", $active).each(function () {
 								var $ui = $(this),
 									$dls = $("dl", $ui),
 									$dt = $("dt", $ui);
-								$dt.on("click", function() {
+								$dt.on("click", function () {
 									var $dl = $(this).closest("dl"),
 										$next = $dl.next("dl"),
 										scrollT = 0;
@@ -242,12 +260,12 @@ var publish = function () {
 										}, {
 											duration: 600,
 											queue: true,
-											complete: function() {
+											complete: function () {
 												$next.addClass("on");
 											}
 										});
 									} else {
-										setTimeout(function() {
+										setTimeout(function () {
 											$html.addClass("chat-done");
 											swiper.slideNext();
 										}, 800);
@@ -291,7 +309,7 @@ $(function () {
 
 	$.fn.grandparent = function (recursion) {
 		if (recursion == undefined) recursion = 2;
-		if (typeof(recursion) == "number") {
+		if (typeof (recursion) == "number") {
 			recursion = parseInt(recursion);
 			if (recursion > 0) {
 				grandsome = $(this);
